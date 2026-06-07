791a14ee9ea7353f510e449acb010299b73f0f5e
Petr Makhnev <51853996+i582@users.noreply.github.com>
2026-06-07 20:47:40 +0400

    cleanup

--- src/cli.ts
+++
@@ -1,4 +0,0 @@
-import { loadAbiRegistry } from "./load-schemas.js";
-
-const registry = await loadAbiRegistry();
-process.stdout.write(`${JSON.stringify(registry, null, 2)}\n`);
--- src/index.ts
+++
@@ -1,9 +0,0 @@
-export { loadAbiRegistry } from "./load-schemas.js";
-export {
-  CONTRACT_CODE_HASHES_FILE_NAME,
-  generateContractCodeHashesJson,
-  generateContractCodeHashesMap,
-  generateTolkTypesFiles,
-  writeTolkTypesFiles,
-} from "./generate-tolk.js";
-export type * from "./types.js";
